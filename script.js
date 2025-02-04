9f9652239113335cbda6d3de402bb6726e066e98
Kazue Kurosaki <137025030+KazueKuroshi@users.noreply.github.com>
2025-02-05 01:56:56 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
- // Menampilkan tombol "Kembali ke Atas" saat menggulir
-    window.onscroll = function() {
-        const backToTopButton = document.getElementById('backToTop');
-        if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-            backToTopButton.style.display = "block";
-        } else {
-            backToTopButton.style.display = "none";
-        }
-    };
+// Menampilkan tombol "Kembali ke Atas" saat menggulir
+window.onscroll = function() {
+    const backToTopButton = document.getElementById('backToTop');
+    if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
+        backToTopButton.style.display = "block";
+    } else {
+        backToTopButton.style.display = "none";
+    }
+};
 
-    // Mengatur fungsi untuk kembali ke atas saat tombol diklik
-    document.getElementById('backToTop').onclick = function() {
-        window.scrollTo({top: 0, behavior: 'smooth'});
-    };
+// Mengatur fungsi untuk kembali ke atas saat tombol diklik
+document.getElementById('backToTop').onclick = function() {
+    window.scrollTo({top: 0, behavior: 'smooth'});
+};
+
+function searchFunction() {
+            const input = document.getElementById('searchInput');
+            const filter = input.value.toLowerCase();
+            const productItems = document.querySelectorAll('.product-item');
+
+            productItems.forEach(item => {
+                const title = item.querySelector('h3').textContent.toLowerCase();
+                if (title.includes(filter)) {
+                    item.style.display = ""; // Tampilkan item
+                } else {
+                    item.style.display = "none"; // Sembunyikan item
+                }
+            });
+        }
